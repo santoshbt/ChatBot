64b26451e1eff8ced68072b78e0fbfd75745f7fd
llssbb <bt.santosh01@gmail.com>
2024-05-11 23:29:50 +0100

    using conversational_rag_chain and chat history

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from src.helper import load_embedding
 from dotenv import load_dotenv
 import os
 from src.helper import extract_text, validate_url, clear_previous_data
 from flask import Flask, render_template, jsonify, request
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationSummaryMemory
-from langchain.chains import ConversationalRetrievalChain
-from langchain.prompts import PromptTemplate
-
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from src.helper import load_embedding
 
 app = Flask(__name__)
 
@@ -23,27 +31,67 @@ persist_directory = "db"
 vectordb = Chroma(persist_directory=persist_directory,
                   embedding_function=embeddings)
 
-custom_prompt_template = """
-You are a helpful assistant who answer politely to the customer queries.
-Focus only on the information related to insurance domain.
-Always answer in a step by step manner.
-if any outside context query is asked, respond as Sorry, I am not aware of it.
-{context}
+def qa(input):
+    retriever = vectordb.as_retriever()
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-Question: {question}
-"""
+    ### Contextualize question ###
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+        which might reference context in the chat history, formulate a standalone question \
+        which can be understood without the chat history. Do NOT answer the question, \
+        just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
-custom_prompt = PromptTemplate(
-    template=custom_prompt_template,
-    input_variables=["context", "question"],
-)
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
 
-llm = ChatOpenAI()
-memory = ConversationSummaryMemory(llm=llm, memory_key = "chat_history", return_messages=True)
-qa = ConversationalRetrievalChain.from_llm(llm,
-                                           retriever=vectordb.as_retriever(search_type="mmr", search_kwargs={"k":3}),
-                                           combine_docs_chain_kwargs={"prompt": custom_prompt},
-                                           memory=memory)
+    qa_system_prompt = """You are an assistant for question-answering tasks on insurance domain. \
+        Use the following pieces of retrieved context to answer the question. \
+        If you don't know the answer, just say that you don't know. \
+        Use three sentences maximum and keep the answer concise.\
+
+        {context}"""
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", qa_system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    conversational_rag_chain = RunnableWithMessageHistory(
+                                    rag_chain,
+                                    get_session_history,
+                                    input_messages_key="input",
+                                    history_messages_key="chat_history",
+                                    output_messages_key="answer",
+                                )
+
+    result = conversational_rag_chain.invoke(
+        {"input": input},
+        config={
+            "configurable": {"session_id": "abc123"}
+        },  # constructs a key "abc123" in `store`.
+    )
+    return result['answer']
+
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    store = {}
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -72,7 +120,7 @@ def chat():
         return "Thanks for contacting, please paste the new web URL if you want to continue"
     else:
         result = qa(input)
-        return str(result["answer"])
+        return str(result)
 
 
 if __name__ == '__main__':
